6c8f55270ca4c312a70e7ca06cae1a8a44e283f4
DarioGiusino <dario.giusino@gmail.com>
2023-01-23 15:04:28 +0100

    appHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,26 +8,58 @@ export default {
 </script>
 
 <template>
-    <header>
-        <div class="container">
-            <figure>
-                <img src="" alt="">
-            </figure>
+    <header class="container">
+        <figure>
+            <img src="../assets/img/dc-logo.png" alt="Dc logo">
+        </figure>
 
-            <nav>
-                <ul>
-                    <li v-for="link in links" :key="link.text">{{ link.text }}</li>
-                </ul>
-            </nav>
-        </div>
+        <nav>
+            <ul>
+                <li v-for="link in links" :key="link.text"><a :href="link.url" :class="{ 'current': link.current }">{{
+                    link.text
+                }}</a></li>
+            </ul>
+        </nav>
     </header>
 </template>
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
+@use '../assets/scss/partials/mixins' as *;
 
 header {
-    height: $height-s;
-    border: 2px solid red;
+    @include flex('between', 'center');
+    height: 100px;
+    margin: 20px auto;
+
+    nav {
+        height: 100%;
+
+        ul {
+            @include list-reset;
+            @include flex('around', 'center');
+            height: 100%;
+
+            li {
+                @include flex('center', 'center');
+                height: 100%;
+
+                a {
+                    @include flex('center', 'center');
+                    font-weight: bold;
+                    font-size: 14px;
+                    height: 100%;
+                    padding: 0px 10px;
+                    border-bottom: 5px solid transparent;
+                }
+
+            }
+        }
+    }
+}
+
+.current {
+    color: $blue;
+    border-color: $blue;
 }
 </style>
